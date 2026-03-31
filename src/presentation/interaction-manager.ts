e73a65e7812972ef6d1e1a8d02c50ae18d6f27af
varunkarmikanda-codecraft <varun.karmikanda@codecraft.co.in>
2026-03-31 10:51:29 +0000

    refactor: restructure interaction manager for improved readability and organization

--- src/presentation/interaction-manager.ts
+++ src/presentation/interaction-manager.ts
@@ -2,25 +2,9 @@ import * as readline from 'node:readline';
 import { stdin as input, stdout as output } from "node:process";
 import type { ValidatorFn } from '../core/validators/validator.type.js';
 
-const rl = readline.createInterface({ input, output })
-
 export interface AskOptions {
   defaultAnswer?: string | undefined;
   validator?: ValidatorFn | undefined;
-}
-
-export const ask = async (question: string, options?: AskOptions) => {
-  const { defaultAnswer, validator } = options || {};
-
-    return new Promise((resolve) => {
-        rl.question(defaultAnswer ? `${question} (${defaultAnswer}) ` : `${question} `, (answer: string) => {
-            if (validator && !validator(answer)) {
-                console.log('Invalid input. Please try again.');
-                return resolve(ask(question, { defaultAnswer: defaultAnswer, validator: validator }));
-            }
-            resolve(answer || defaultAnswer);
-        });
-    });
 }
 
 export interface Choice {
@@ -28,14 +12,42 @@ export interface Choice {
   value: string;
 }
 
-export const choose = async (question: string, choices: Choice[]) => {
-  console.log(question);
-  choices.forEach((choice) => {
-    console.log(`${choice.value}. ${choice.label}`);
-  })
-  const validator: ValidatorFn = (input: string) => {
-    return choices.some(choice => choice.value.toLowerCase() === input.toLowerCase());
-  }
-  return ask('Please enter a choice', { validator });
-}
+export const openInteractionManager = () => {
 
+  const rl = readline.createInterface({ input, output })
+
+  const ask = async (question: string, options?: AskOptions) => {
+  const { defaultAnswer, validator } = options || {};
+
+    return new Promise((resolve) => {
+      rl.question(defaultAnswer ? `${question} (${defaultAnswer}) ` : `${question} `, (answer: string) => {
+          if (validator && !validator(answer)) {
+              console.log('Invalid input. Please try again.');
+              return resolve(ask(question, { defaultAnswer: defaultAnswer, validator: validator }));
+          }
+          resolve(answer || defaultAnswer);
+      });
+    });
+  }
+
+  const choose = async (question: string, choices: Choice[]) => {
+    console.log(question);
+    choices.forEach((choice) => {
+      console.log(`${choice.value}. ${choice.label}`);
+    })
+    const validator: ValidatorFn = (input: string) => {
+      return choices.some(choice => choice.value.toLowerCase() === input.toLowerCase());
+    }
+    return ask('Please enter a choice', { validator });
+  }
+
+  const close = () => {
+    rl.close();
+  }
+
+  return {
+    ask,
+    choose,
+    close
+  }
+}
